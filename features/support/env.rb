c52b63676a7cd76b4f256223b27ce4e8f86993dd
Kevin Gilpin <kgilpin@gmail.com>
2013-10-31 16:14:39 -0400

    we don't need to print the config

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,5 @@ require 'rspec'
 require 'conjur/api'
 require 'conjur/config'
 
-puts JSON.parse(File.read('conjur.json')).symbolize_keys
-
 Conjur::Config.merge JSON.parse(File.read('conjur.json'))
 Conjur::Config.apply
